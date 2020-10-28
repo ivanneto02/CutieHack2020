4739edc6ef244a570ab9ac0136c7af3ae49f323a
Ivan Neto <72827265+ivanneto02@users.noreply.github.com>
2020-10-27 17:00:49 -0700

    added main

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # CutieHack2020
 Group project for Cutie Hack Competition
-
+#
 Members:
 1. Siyun Zuo
 2. Ainaz Estiri
 3. Ivan Neto
 4. Joel Charco Lopez
+
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<string>
+
+int main() {
+    int integer;
+    /* i need to do a 3pg history paper by friday
+    how do u save it? save as what yeah /main.cpp
+    // xD
+    */
+}
